c2fcc6e9c19d69198aee52b6e05043e271d93c1f
Madan <58905767+Madanaa@users.noreply.github.com>
2020-11-05 11:30:52 -0800

    React With Node.js
    This is an Example project

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return(
+        <div className="header">
+            <h1> Create Project React With NodeJS</h1>
+        </div>
+    )
+}
